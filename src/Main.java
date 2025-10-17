5575566e180888c8d28d3b85867448aa467d9fc6
PaulingCavalcante <ph.doamaralcavalcante@gmail.com>
2025-10-16 21:33:31 -0300

    fix: Correct index found message logic in binary search implementation

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         Object vetor[] = new Object[100];
         int indexInicio = 0, num = 0, contador = 0;
         int indexFim = vetor.length - 1;
         int meio = (indexFim / 2);
-
+        boolean teste = false;
         num = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual índice você está buscando?"));
 
-        while (meio < indexFim){
-            if (num == meio){
-                JOptionPane.showMessageDialog(null,"Índex encontrado: " + meio + " em " + contador + " iterações");
+        while (meio < indexFim) {
+            if (num == meio) {
+                teste = true;
                 break;
-            } 
-            else if (num > meio){
+            } else if (num > meio) {
                 indexInicio = (meio + 1);
                 System.out.println("Índex > " + meio + " Iteração nº: " + (contador + 1));
             } else {
@@ -23,5 +23,7 @@ public class Main {
             contador++;
             meio = ((indexInicio + indexFim) / 2);
         }
+        if(teste) JOptionPane.showMessageDialog(null, "Índex encontrado: " + meio + " em " + contador + " iterações");
+        else JOptionPane.showMessageDialog(null, "Índex não encontrado");
     }
 }
